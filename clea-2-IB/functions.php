581648fe0acf90c597d271c2cf75693decf86068
Anne-Laure Delpech <contact@parcours-performance.com>
2016-11-06 15:28:57 +0100

    strong testimonials settings moved to a plugin

--- clea-2-IB/functions.php
+++ clea-2-IB/functions.php
@@ -19,14 +19,6 @@
 // Do theme setup on the 'after_setup_theme' hook.
 add_action( 'after_setup_theme', 'clea_ib_theme_setup', 11 ); 
 
-// ****** SHOULD BE IN A PLUGIN, together with the spécific style ***********
-// functions for strong testimonials orientation taxonomy
-add_filter( 'wpmtst_query_args', 'clea_ib_strong_testimonials_query_args' );
-add_action( 'save_post_wpm-testimonial', 'clea_ib_default_tax_slug_strong_testimonials' );
-// add a new taxonomy to strong testimonials
-add_action( 'init', 'clea_ib_add_taxonomy_to_strong_testimonial', 11 );	
-	
-
 function clea_ib_theme_setup() {
 	/* Register and load styles and scripts. */
 	add_action( 'wp_enqueue_scripts', 'clea_ib_enqueue_styles_scripts', 4 ); 
@@ -49,84 +41,18 @@ function clea_ib_enqueue_styles_scripts() {
 		wp_enqueue_style( 'clea-ib-quiz-master', get_stylesheet_directory_uri() . '/css/clea-ib-quiz-and-survey-master.css', array(), false, 'all' );
 		
 	} 
+
 	// feuille de style pour Strong Testimonials - https://fr.wordpress.org/plugins/strong-testimonials/
 	if( is_plugin_active( 'strong-testimonials/strong-testimonials.php' ) ) {
 		
 		wp_enqueue_style( 'clea-ib-strong-testimonials', get_stylesheet_directory_uri() . '/css/clea-ib-strong-testimonials.css', array(), false, 'all' );
-	}
+	}	
+	
 	// font awesome CDN
 	wp_enqueue_script( 'clea-ib-font-awesome', 'https://use.fontawesome.com/1dcb7878fd.js', false );
 	
 }
-function clea_ib_add_taxonomy_to_strong_testimonial() {
-	
-	// for 'post_types' => array( 'wpm-testimonial' ),
-  $labels = array(
-		'name'              => __( 'Orientations', 'clea-2-IB' ),
-		'singular_name'     => __( 'Orientation', 'clea-2-IB' ),
-		'search_items'      => __( 'Chercher dans les Orientations', 'clea-2-IB' ),
-		'all_items'         => __( 'Toutes les orientations', 'clea-2-IB' ),
-		'edit_item'         => __( 'Modifier l\'orientation', 'clea-2-IB' ),
-		'update_item'       => __( 'Mettre à jour l\'orientation', 'clea-2-IB' ),
-		'add_new_item'      => __( 'Ajouter une nouvelle  orientation', 'clea-2-IB' ),
-		'new_item_name'     => __( 'Nom de la nouvelle orientation', 'clea-2-IB' ),
-		'menu_name'         => __( 'Orientations', 'clea-2-IB' ),
-	);
-	
-	$args = array(
-		'labels' => $labels,
-		'hierarchical' 		=> true,
-		'sort' 				=> true,
-		'args' 				=> array( 'orderby' => 'term_order' ),
-		'rewrite' 			=> array( 'slug' => 'orientation-tag' ),
-		'show_admin_column' => true,
-		'default_term'		=> 'orientation-complet'
-	);
-    // register the taxonomy
-    register_taxonomy( 'orientation', 'wpm-testimonial', $args );
-	
-}
-function clea_ib_strong_testimonials_query_args( $args ) {
-	/* using the term ID: */
-	
-	/*
-	$args['tax_query'] = array(
-		array(
-			'taxonomy' => 'orientation',
-			'field'    => 'id',
-			'terms'    => 123
-		)
-	);
-	*/
-	if ( is_page( 914 ) ) {
-		
-		$orientation_tag_slug = 'orientation-isabelle' ;
-	} else {
-		
-		$orientation_tag_slug = 'orientation-complet' ;
-		
-	}
-	/* using the term slug: */
-	$args['tax_query'] = array(
-		array(
-			'taxonomy' => 'orientation',
-			'field'    => 'slug',
-			'terms'    => $orientation_tag_slug
-		)
-	);	
-	return $args;
-}
-function clea_ib_default_tax_slug_strong_testimonials( $post_id ){
-		
-	// http://wordpress.stackexchange.com/questions/7168/how-to-add-a-default-item-to-a-custom-taxonomy
-	// will set the default orientation taxonomy term to "orientation-complet" for 
-	// all Strong testimonial custom-post-types (wpm-testimonial) 
-	$current_post = get_post( $post_id );
-	// This makes sure the taxonomy is only set when a new post is created
-	if ( $current_post->post_date == $current_post->post_modified ) {
-		wp_set_object_terms( $post_id, 'orientation-complet', 'orientation', true );
-	}		
-}
+
 /**********************************************
 * display 4 metaboxes on frontpage
 * source http://help4cms.com/add-wysiwyg-editor-in-wordpress-meta-box/
@@ -156,7 +82,40 @@ function clea_ib_frontpage_wysiwyg_meta_box()  {
 				   'low',
 				   array( 'editor' => 'editor_section_1',
 						'template' => $template_file ) // will be in $box
-			  );	
+			  );
+
+			add_meta_box( 
+				   'edit_section_2',
+				   __('Section 2', 'clea-2-IB'),
+				   'clea_ib_custom_wysiwyg',
+				   'page',
+				   'normal',
+				   'low',
+				   array( 'editor' => 'editor_section_2',
+						'template' => $template_file ) // will be in $box
+			  );
+
+			add_meta_box( 
+				   'edit_section_3',
+				   __('Section 3', 'clea-2-IB'),
+				   'clea_ib_custom_wysiwyg',
+				   'page',
+				   'normal',
+				   'low',
+				   array( 'editor' => 'editor_section_3',
+						'template' => $template_file ) // will be in $box
+			  );
+
+			add_meta_box( 
+				   'edit_section_4',
+				   __('Section 4', 'clea-2-IB'),
+				   'clea_ib_custom_wysiwyg',
+				   'page',
+				   'normal',
+				   'low',
+				   array( 'editor' => 'editor_section_4',
+						'template' => $template_file ) // will be in $box
+			  );			  
 	}
 }
 
@@ -167,7 +126,7 @@ function clea_ib_custom_wysiwyg( $post, $args )  {
 
 	    // print_r($post);
 
-	    print_r($args);
+	    // print_r($args);
 		/* 
 		Array(
 			[id] => edit_section_1
@@ -183,7 +142,6 @@ function clea_ib_custom_wysiwyg( $post, $args )  {
 	    echo '</pre>';
 		
 	$content = get_post_meta($post->ID, $args['args']['editor'] , true);
-
 	wp_editor(
 		htmlspecialchars_decode( $content ) ,
 		$args['args']['editor'], 
@@ -193,12 +151,27 @@ function clea_ib_custom_wysiwyg( $post, $args )  {
 	);
 }
  
-function clea_ib_custom_wysiwyg_save_postdata($post_id )  {
+function clea_ib_custom_wysiwyg_save_postdata( $post_id )  {
  
 	if (!empty($_POST['editor_section_1']))  {
 		
 		$data = htmlspecialchars($_POST['editor_section_1']);
 		update_post_meta($post_id, 'editor_section_1', $data);
+	}
+	if (!empty($_POST['editor_section_2']))  {
+		
+		$data = htmlspecialchars($_POST['editor_section_2']);
+		update_post_meta($post_id, 'editor_section_2', $data);
+	}
+	if (!empty($_POST['editor_section_3']))  {
+		
+		$data = htmlspecialchars($_POST['editor_section_3']);
+		update_post_meta($post_id, 'editor_section_3', $data);
+	}
+	if (!empty($_POST['editor_section_4']))  {
+		
+		$data = htmlspecialchars($_POST['editor_section_4']);
+		update_post_meta($post_id, 'editor_section_4', $data);
 	}
 }
 
